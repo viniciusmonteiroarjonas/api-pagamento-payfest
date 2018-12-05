cafb0679a3ab4a08ac117c727c536ff24139820e
vinicius monteiro arjonas <viniciusmonteiroarjonas@outlook.com>
2018-12-04 23:01:36 -0200

    implementado um cliente SOAP e consumindo webservice do correio apra calcular prazo

---
+++ payfest/servicos/correiosSOAPClient.js
@@ -0,0 +1,22 @@
+var soap = require('soap');
+
+function CorreiosSOAPClient(){
+    this._url = 'http://ws.correios.com.br/calculador/calcprecoprazo.asmx?wsdl';
+}
+
+module.exports = function(){
+    return CorreiosSOAPClient;
+}
+
+CorreiosSOAPClient.prototype.calculaPrazo = function(args, callback){
+
+    soap.createClient(this._url, function(erro, cliente){
+        console.log('Cliente SOAP criado');
+    
+        cliente.CalcPrazo(args,callback);
+    
+    });
+
+}
+
+
